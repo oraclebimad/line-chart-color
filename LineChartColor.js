5d1c0bc2a5904aaa9421306ae424d44b8f4024ad
Sergio Medina <sergio.medina@oracle.com>
2015-07-14 17:00:36 -0500

    Added plugin description

--- LineChartColor.js
+++ LineChartColor.js
@@ -3,6 +3,7 @@
   component: {
     'name': 'Line Bar Chart',
     'tooltip': 'Line Bar Chart is a variation of Bar chart, each bar is represented as a line with label and data values positioned right on top of the line. You can scroll up or down to see more data points. It supports hierarchical data filtering so that you can tap or click on each Line Bar to drill down to the next level.',
+    'description': 'Line Bar Chart is a variation of Bar chart, each bar is represented as a line with label and data values positioned right on top of the line. You can scroll up or down to see more data points. It supports hierarchical data filtering so that you can tap or click on each Line Bar to drill down to the next level.',
     'cssClass': 'line-chart-plugin',
     'icon': 'asset://official-plugin.png'
   },
